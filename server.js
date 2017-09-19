1fa9e269fb9ab2a271797b0122360724d8330123
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-09-19 08:47:17 -0700

    added structure and basic css

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 2080;
+
+app.use(express.static('public'));
+
+app.listen(PORT, function(){
+	console.log('express listening on port ' + PORT);
+});
